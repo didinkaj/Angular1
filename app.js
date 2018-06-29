3581394b71db733866bed265b4392651eea801e2
Johnson Didinya <jdidinya@Cytonns-MacBook-Pro-2.local>
2018-06-29 12:22:41 +0300

    filters with angular

--- app.js
+++ app.js
@@ -2,51 +2,43 @@
 
 var app = angular.module('toDoApp', ['ngRoute']);
 
-app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-
-    when('/calculator', {
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/calculator', {
         templateUrl: 'calculator.html',
         controller: 'calculatorController'
-    }).
-
-    when('/tasks', {
+    }).when('/tasks', {
         templateUrl: 'tasks.html',
         controller: 'toDoController'
-    }).
-
-    when('/', {
+    }).when('/', {
         templateUrl: 'index.html',
-    }).
-
-    otherwise({
+    }).otherwise({
         redirectTo: '/'
     });
 }]);
 
-app.controller('calculatorController', ['$scope', function($scope){
-    
-}]);
-    //Controller
-app.controller('toDoController', ['$scope', function($scope) {
+app.controller('calculatorController', ['$scope', function ($scope) {
 
-            //Scope
+}]);
+//Controller
+app.controller('toDoController', ['$scope', function ($scope) {
+
+    //Scope
     $scope.tasks = [{
-        id:1,
-        name:'study angular',
-        date:"27/6/2018"
+        id: 1,
+        name: 'study angular',
+        date: "27/6/2018"
 
     },
         {
-            id:2,
-            name:'study node',
-            date:"27/6/2018"
+            id: 2,
+            name: 'study node',
+            date: "27/6/2018"
 
         },
         {
-            id:3,
-            name:'Project a',
-            date:"27/6/2018"
+            id: 3,
+            name: 'Project a',
+            date: "27/6/2018"
 
         }
     ];
@@ -55,37 +47,37 @@ app.controller('toDoController', ['$scope', function($scope) {
     $scope.newTaskName = '';
 
 
- //adding a task to task list
+    //adding a task to task list
 
-    $scope.addTask = function() {
+    $scope.addTask = function () {
 
         var name = $scope.newTaskName;
 
-        let id = this.tasks.length +1;
+        let id = this.tasks.length + 1;
 
         let date = new Date();
 
         if (name && $scope.tasks.indexOf(name) == -1 && $scope.completed.indexOf(name)) {
 
             $scope.tasks.push({
-                name:name,
-                id:id,
-                date:date
+                name: name,
+                id: id,
+                date: date
             });
 
             $scope.newTaskName = '';
         }
     };
 //-----deleting a task
-    $scope.deleteTask = function(task){
+    $scope.deleteTask = function (task) {
 
         this.tasks.pop(task);
 
     }
 
- //-------------scope-------- using inbuilt array functions push and splice- add/remove items from array and return removed items
+    //-------------scope-------- using inbuilt array functions push and splice- add/remove items from array and return removed items
 
-    $scope.markcomplete = function(index, completedTaskArray, pendingTaskArray) {
+    $scope.markcomplete = function (index, completedTaskArray, pendingTaskArray) {
 
         pendingTaskArray.push(completedTaskArray[index]);
 
@@ -93,17 +85,15 @@ app.controller('toDoController', ['$scope', function($scope) {
     }
 
 
-
 }]);
 
 // Add Custom filters
 
 
-app.filter('kenyashilling', function() {
+app.filter('kenyashilling', function () {
 
 
-    return function(number) {
-
+    return function (number) {
 
 
         return 'ksh' + number;
